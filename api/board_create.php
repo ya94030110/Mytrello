8a10bfebeeb8645d219277a9a35b33393db75189
b03902087 <b03902087@ntu.edu.tw>
2019-01-07 17:29:24 +0800

    commit create board api

---
+++ api/board_create.php
@@ -0,0 +1,31 @@
+<?php
+require_once "../utility.php";
+require_once "../database.php";
+if(isset($_POST['boardid'], $_POST['title'], $_POST['board_len'])) {  //checking the request
+  // calling database.php function to establish the connection to MySQL server
+  $conn = connectOurtubeDatabase();
+  // print the data for debug
+  // echo $_POST['data'];
+  $boardid = $_POST['boardid'];
+  $title = $_POST['title'];
+  $board_len = $_POST['board_len'];
+  $success = 0;
+  $result = insertVideoCaption(
+    $boardid,
+    $title,
+    $board_len,
+    $conn
+  );
+  if($result === True) {
+    $success = 1;
+  } 
+  else {
+    $success = 0;
+  }
+  $ret = [
+    'success' => $success,
+  ];
+  
+  exit(json_encode($ret));
+}
+?>
