3c8a6c2b1dbdf07f359bd1daa2d5d6d20b824e01
b03902087 <b03902087@ntu.edu.tw>
2019-01-11 22:07:40 +0800

    remove debug message

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -23,7 +23,6 @@ var tools = (function(){
                 }
             ).done(function(res){
                 res = tools.json_preprocess(res);
-                console.log(res);
                 response = JSON.parse(res);
                 tools.constructHTML(response);
             })
